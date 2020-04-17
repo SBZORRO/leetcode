4f2bc2bc5dd9e7c358b5d09dcb3264d9ddad41b1
lianghao <lianghao@everyoo.com>
2020-04-17 17:31:54 +0800

    04/17

--- src/main/java/AllSorting.java
+++ src/main/java/AllSorting.java
@@ -6,7 +6,75 @@ public class AllSorting {
     AllSorting test = new AllSorting();
 //    System.out.println(test.insertion(new int[] { 2, 1, 8, 6, 3, 7, 9 }));
 //    System.out.println(test.selection(new int[] { 2, 1, 8, 6, 3, 7, 9 }));
-    System.out.println(test.merge(new int[] { 2, 1, 8, 6, 3, 7, 9 }));
+//    System.out.println(test.merge(new int[] { 2, 1, 8, 6, 3, 7, 9 }));
+//    System.out.println(test.quick(new int[] { 2, 1, 8, 6, 3, 7, 9 }));
+  }
+
+  public int[] quick(int[] a) {
+    int pivot = pivot(a);
+    int tmp = 0;
+    for (int i = 0, j = a.length - 2; i < j;) {
+      while (a[i] < pivot) {
+        i++;
+      }
+      while (a[j] > pivot) {
+        j--;
+      }
+      if (i >= j) {
+        tmp = a[i];
+        a[i] = a[a.length - 1];
+        a[a.length - 1] = tmp;
+        int[] l = quick(Arrays.copyOfRange(a, 0, i));
+        int[] r = quick(Arrays.copyOfRange(a, i, a.length));
+
+        for (int m = 0; m < l.length; m++) {
+          a[m] = l[m];
+        }
+        for (int m = l.length, n = 0; n < r.length && m < a.length; m++, n++) {
+          a[m] = r[n];
+        }
+        return a;
+      }
+
+      tmp = a[i];
+      a[i] = a[j];
+      a[j] = tmp;
+    }
+    return a;
+  }
+
+  public int pivot(int[] a) {
+    int pivot = 0;
+    int tmp = 0;
+    int[] tbd = new int[3];
+    tbd[0] = a[0];
+    tbd[1] = a[a.length / 2];
+    tbd[2] = a[a.length - 1];
+    if (tbd[0] <= Math.min(tbd[1], tbd[2])) {
+      if (tbd[1] <= tbd[2]) {
+        pivot = a.length / 2;
+      } else {
+        pivot = a.length - 1;
+      }
+    }
+    if (tbd[1] <= Math.min(tbd[0], tbd[2])) {
+      if (tbd[0] <= tbd[2]) {
+        pivot = 0;
+      } else {
+        pivot = a.length - 1;
+      }
+    }
+    if (tbd[2] <= Math.min(tbd[1], tbd[0])) {
+      if (tbd[1] <= tbd[0]) {
+        pivot = a.length / 2;
+      } else {
+        pivot = 0;
+      }
+    }
+    tmp = a[pivot];
+    a[pivot] = a[a.length - 1];
+    a[a.length - 1] = tmp;
+    return a[a.length - 1];
   }
 
   public int[] insertion(int[] a) {
--- src/main/java/No56_MergeInterval.java
+++ src/main/java/No56_MergeInterval.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class No56_MergeInterval {
 //  Given a collection of intervals, merge all overlapping intervals.
 //
@@ -20,11 +23,62 @@ public class No56_MergeInterval {
 //  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
   public static void main(String[] args) {
     No56_MergeInterval test = new No56_MergeInterval();
-    System.out.println(test.merge("tmmzuxt"));
+    System.out.println(test.merge(new int[][] { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } }));
+    System.out.println(test.merge(new int[][] { { 1, 4 }, { 4, 5 } }));
+    System.out.println(test.merge(new int[][] { { 1, 3 }, { 4, 5 } }));
+    System.out.println(test.merge(new int[][] { { 1, 3 }, { 0, 0 } }));
+    System.out.println(test.merge(new int[][] { { 1, 3 }, { 3, 3 }, { 4, 7 } }));
+    System.out
+        .println(test.merge(new int[][] { { 2, 3 }, { 5, 5 }, { 2, 2 }, { 3, 4 }, { 3, 4 } }));
 
   }
 
   public int[][] merge(int[][] intervals) {
+    int len = 0;
+    for (int i = 0; i < intervals.length; i++) {
+      len = Math.max(len, intervals[i][1]);
+    }
+    int[] res = new int[len + 1];
 
+    for (int i = 0; i < intervals.length; i++) {
+      int[] item = intervals[i];
+      if (item[0] == item[1] && res[item[0]] == 0) {
+        res[item[0]] = 2;
+        continue;
+      }
+      for (int j = item[0]; j < item[1]; j++) {
+        res[j] = 1;
+      }
+    }
+    List<int[]> list = new LinkedList<>();
+    int i = 0;
+
+    while (i <= len) {
+      if (res[i] == 0) {
+        i++;
+        continue;
+      }
+      int[] item = new int[2];
+      item[0] = i;
+      if (res[i] == 2) {
+        item[1] = i;
+        i++;
+      } else {
+        while (i <= len && res[i] == 1) {
+          i++;
+        }
+        item[1] = i;
+        if (res[i] == 2) {
+          i++;
+        }
+      }
+      list.add(item);
+
+    }
+    int[][] ans = new int[list.size()][2];
+    for (int j = 0; j < list.size(); j++) {
+      ans[j] = list.get(j);
+    }
+    return ans;
   }
 }
